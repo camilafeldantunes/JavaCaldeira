e95268591bb0070faaaabca00ff162c3f3186f44
camila2601 <99697225+camila2601@users.noreply.github.com>
2023-10-04 09:08:24 -0300

    Primeiro exercício feito

---
+++ Exercicio01/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner nome = new Scanner(System.in);
+        System.out.println("Digite o seu nome");
+        String SeuNome = nome.nextLine();
+        System.out.println(SeuNome);
+    }
+}
