75477592055f351cdb5d5dba2bdde1d84e5b1ac8
camila2601 <99697225+camila2601@users.noreply.github.com>
2023-10-04 09:51:42 -0300

    Exercicio 04 modificado e pronto

--- Exercicio01/src/Exercicio4.java
+++ Exercicio01/src/Exercicio4.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Exercicio4 {
 
     public static void main(String[] args){
-        final int salarioMinimo = 1320;
+        final double salarioMinimo = 1320;
         Scanner salario = new Scanner(System.in);
         System.out.println("Digite o seu salário");
-        int seuSalario = salario.nextInt();
-        int diferencaSalarial = seuSalario - salarioMinimo;
-        System.out.println("A sua diferença salarial quando comparado ao salário mínimo é de R$" + diferencaSalarial);
+        double seuSalario = salario.nextInt();
+        double quantidadeDeSalariosMinimos = seuSalario / salarioMinimo;
+        System.out.println("A quantidade de salários mínimos que você ganha é de " + quantidadeDeSalariosMinimos);
 
 
     }
